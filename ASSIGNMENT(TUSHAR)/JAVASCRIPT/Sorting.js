bfd01e25311e480e5d6f8a3e86914fc93647f25a
Tushar <55013568+tushar76@users.noreply.github.com>
2023-10-12 14:32:52 +0530

    Update Sorting.js

--- ASSIGNMENT(TUSHAR)/JAVASCRIPT/Sorting.js
+++ ASSIGNMENT(TUSHAR)/JAVASCRIPT/Sorting.js
@@ -1,7 +1,14 @@
-const a1 = [2, 4, 6, 8, 10, 12, 14];
-function arraySort(arr) {
-  arr.sort((c, d) => c - d);
-  arr.reverse();
-  return arr;
+function sort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] < arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
-console.log(arraySort(a1));
+const nums = [2,3,4,5,6,7,8];
+sort(nums);
+console.log(nums); 
